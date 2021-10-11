755e3123bcab6e5700b2503a8040cdcc83153f14
pk <1194206601@qq.com>
2021-10-11 21:13:43 +0800

    juc

--- workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Market.java
+++ workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Market.java
@@ -1,4 +1,21 @@
 package com.pk.juc.sync;
 
+import java.util.concurrent.TimeUnit;
+
 public class Market {
+
+    public final String saleLock="saleLock";
+    public final String purchaseLock="purchaseLock";
+
+    public  void sale() {
+        synchronized (saleLock){
+        System.out.println("sale");
+        }
+    }
+
+    public void purchase() {
+        synchronized (purchaseLock){
+            System.out.println("Purchase");
+        }
+    }
 }
--- workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Test.java
+++ workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Test.java
@@ -1,4 +1,9 @@
 package com.pk.juc.sync;
 
 public class Test {
+    public static void main(String[] args) {
+        Market market = new Market();
+        new Thread(market::purchase).start();
+        new Thread(market::sale).start();
+    }
 }
