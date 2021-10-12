0a025c32903bbe93ab3e34ae37d5bb3965d9d0a6
pk <1194206601@qq.com>
2021-10-12 22:39:29 +0800

    java

---
+++ workspace/Java/java-mockingbird/src/main/java/com/pk/juc/prodcos/Goods.java
@@ -0,0 +1,29 @@
+package com.pk.juc.prodcos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 商店，包含进货和出货
+ */
+public class Goods {
+
+    private List<String> foodList = new ArrayList<>();
+
+    private void add(String food) {
+        synchronized (this) {
+            if (foodList.size() > 5) {
+                this.notify();
+            }
+        }
+    }
+
+    private String get() throws InterruptedException {
+        synchronized (this) {
+            if (foodList.size() < 1) {
+                this.wait();
+            }
+        }
+    }
+
+}
---
+++ workspace/Java/java-mockingbird/src/main/java/com/pk/juc/prodcos/Producer.java
@@ -0,0 +1,4 @@
+package com.pk.juc.prodcos;
+
+public class Producer {
+}
