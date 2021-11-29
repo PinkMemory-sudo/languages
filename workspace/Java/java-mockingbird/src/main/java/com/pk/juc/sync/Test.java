f51e2b89d6ebbde9fcf2552571cd39b37da2329d
pk <1194206601@qq.com>
2021-11-30 01:05:59 +0800

    多线程

--- workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Test.java
+++ workspace/Java/java-mockingbird/src/main/java/com/pk/juc/sync/Test.java
@@ -2,8 +2,19 @@ package com.pk.juc.sync;
 
 public class Test {
     public static void main(String[] args) {
-        Market market = new Market();
-        new Thread(market::purchase).start();
-        new Thread(market::sale).start();
+        Person person = new Person();
+        Runnable task = () -> {
+            person.test();
+        };
+        new Thread(task, "t1").start();
+        new Thread(task, "t2").start();
+    }
+
+    static class Person {
+        public synchronized void test() {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+        }
     }
 }
